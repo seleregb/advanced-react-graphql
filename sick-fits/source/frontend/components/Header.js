6fc126a6cba6761dc470205686a2e1ebd056947b
seleregb <seleregb@gmail.com>
2019-06-20 16:25:10 -0300

    completed search functionality and code clean up

--- sick-fits/source/frontend/components/Header.js
+++ sick-fits/source/frontend/components/Header.js
@@ -4,6 +4,7 @@ import Router from 'next/router';
 import NProgress from 'nprogress';
 import Nav from './Nav';
 import Cart from './Cart';
+import AutoComplete from './Search';
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -65,7 +66,7 @@ const Header = () => (
       <Nav />
     </div>
     <div className="sub-bar">
-      <p>Search</p>
+      <AutoComplete />
     </div>
     <Cart />
   </StyledHeader>
